e591da7fc060bf2a9620bf13bf67b9f37238e801
Kai Geffen <geffenkai@gmail.com>
2021-03-05 10:55:11 -0500

    Changed cors in webpack to allow game server

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,27 @@ const path = require('path');
 module.exports = {
     entry: './src/app.ts',
     module: {
-	rules: [
-{
-    test: /\.tsx?$/,
-    use: 'ts-loader'
-}
-    ]
+        rules: [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader'
+        }
+        ]
+    },
+    devServer: {
+        headers: {
+            "Access-Control-Allow-Origin": "192.168.1.152"
+        }
     },
     resolve: {
-	    extensions: [ '.ts', '.tsx', '.js' ],
+        extensions: [ '.ts', '.tsx', '.js' ],
         alias: {
             path: require.resolve("path-browserify")
         }
     },
     output: {
-	filename: 'app.js',
-	path: path.resolve(__dirname, 'dist')
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
     },
     mode: 'development'
 };
