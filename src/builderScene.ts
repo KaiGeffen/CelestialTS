991ca500487ec40e9494d851eeb1067822664c6d
Kai Geffen <geffenkai@gmail.com>
2021-03-09 18:09:31 -0500

    Refactored BuilderScene, making style consistent, renaming confusing 'filter' variable

--- src/builderScene.ts
+++ src/builderScene.ts
@@ -4,9 +4,9 @@ import { CardImage, addCardInfoToScene } from "./cardImage"
 import { buttonStyle, filterButtonStyle, space } from "./settings"
 
 
-// Load this from a json shared with python repo
 const catalog = collectibleCards
 
+// The card hover text for this scene, which is referenced in the regions
 var cardInfo: Phaser.GameObjects.Text
 
 
@@ -26,19 +26,14 @@ export class BuilderScene extends Phaser.Scene {
     this.catalogRegion = new CatalogRegion(this, this.deckRegion)
     this.filterRegion = new FilterRegion(this, this.catalogRegion)
 
-    // let style = {
-    //   font: '36px Arial Bold',
-    //   color: '#d00',
-    //   backgroundColor: '#88a',
-    //   wordWrap: { width: 500, useAdvancedWrap: true }
-    // };
     cardInfo = addCardInfoToScene(this)
   }
 
+  // Load all of the card and token images
   preload(): void {
     this.load.setBaseURL(
       "https://raw.githubusercontent.com/KaiGeffen/" +
-      "Celestial/master/images/");
+      "Celestial/master/images/")
 
     catalog.forEach( (card) => {
       this.load.image(card.name, `${card.name}.png`)
@@ -57,34 +52,36 @@ export class BuilderScene extends Phaser.Scene {
 
 
 class CatalogRegion {
-  scene: Phaser.Scene;
-  container: Phaser.GameObjects.Container;
+  scene: Phaser.Scene
+  container: Phaser.GameObjects.Container
   deckRegion
   cardImages: CardImage[] = []
 
   constructor(scene: Phaser.Scene, deckRegion) {
-    this.init(scene, deckRegion);
+    this.init(scene, deckRegion)
   }
 
   init(scene, deckRegion): void {
-    this.scene = scene;
-    this.container = this.scene.add.container(0, 0);
-    this.deckRegion = deckRegion;
+    this.scene = scene
+    this.container = this.scene.add.container(0, 0)
+    this.deckRegion = deckRegion
   }
 
   create(): void {
     for (var i = catalog.length - 1; i >= 0; i--) {
-      this.addCard(catalog[i], i);
+      this.addCard(catalog[i], i)
     }
   }
 
+  // Filter which cards are visible
+  // Only cards for which filterFunction is true are visible
   filter(filterFunction): void {
     let cardsRemoved = false
     let visibleIndex = 0
 
     for (var i = this.cardImages.length - 1; i >= 0; i--) {
       let cardImage = this.cardImages[i]
-      
+
       // This card is present
       if (filterFunction(cardImage.card)) {
         cardImage.image.setVisible(true)
@@ -102,185 +99,184 @@ class CatalogRegion {
       }
       else
       {
-        // If it was visible but now isn't, this filter is removing more cards
+        // If this card was visible but now isn't, this filter is removing more cards
         if (cardImage.image.visible) cardsRemoved = true
 
         cardImage.image.setVisible(false)
       }
-    })
+    }
   }
 
   private onClick(card: Card): () => void {
     return function() {
-      this.deckRegion.addCard(card);
+      this.deckRegion.addCard(card)
     }
   }
 
   private addCard(card: Card, index: number): void {
-    var image: Phaser.GameObjects.Image;
-    var [x, y] = this.getCardPosition(index);
+    var image: Phaser.GameObjects.Image
+    var [x, y] = this.getCardPosition(index)
     
-    image = this.scene.add.image(x, y, card.name);
-    image.setDisplaySize(100, 100);
+    image = this.scene.add.image(x, y, card.name)
+    image.setDisplaySize(100, 100)
 
-    image.setInteractive();
-    image.on('pointerdown', this.onClick(card), this);
+    image.setInteractive()
+    image.on('pointerdown', this.onClick(card), this)
 
-    this.container.add(image);
+    this.container.add(image)
 
     this.cardImages.push(new CardImage(card, image))
   }
 
   private getCardPosition(index: number): [number, number] {
-    let col = index % space.cardsPerRow;
-    let xPad = (1 + col) * space.pad;
-    let x = col * space.cardSize + xPad + space.cardSize / 2;
+    let col = index % space.cardsPerRow
+    let xPad = (1 + col) * space.pad
+    let x = col * space.cardSize + xPad + space.cardSize / 2
 
-    let row = Math.floor(index / space.cardsPerRow);
-    let yPad = (1 + row) * space.pad;
-    let y = row * space.cardSize + yPad + space.cardSize / 2;
+    let row = Math.floor(index / space.cardsPerRow)
+    let yPad = (1 + row) * space.pad
+    let y = row * space.cardSize + yPad + space.cardSize / 2
 
-    return [x, y];
+    return [x, y]
   }
 }
 
 
 class DeckRegion {
-  scene: Phaser.Scene;
-  container: Phaser.GameObjects.Container;
-  deck: CardImage[];
-  btnStart: Phaser.GameObjects.Text;
+  scene: Phaser.Scene
+  container: Phaser.GameObjects.Container
+  deck: CardImage[] = []
+  btnStart: Phaser.GameObjects.Text
 
   constructor(scene: Phaser.Scene) {
-    this.init(scene);
+    this.init(scene)
   }
 
   init(scene): void {
-    this.scene = scene;
-    this.container = this.scene.add.container(1000, 650);
-
-    this.deck = [];
+    this.scene = scene
+    this.container = this.scene.add.container(1000, 650)
   }
 
   create(): void {
     // Sort button 
-    let btnSort = this.scene.add.text(0, -125, 'Sort', buttonStyle);
-    btnSort.setInteractive();
+    let btnSort = this.scene.add.text(0, -125, 'Sort', buttonStyle)
+    btnSort.setInteractive()
 
-    let deckRegion = this;
+    let that = this
     btnSort.on('pointerdown', function (event) {
-      deckRegion.sort();
+      that.sort()
     })
 
-    this.container.add(btnSort);
+    this.container.add(btnSort)
 
     // Start button
-    this.btnStart = this.scene.add.text(0, -75, '', buttonStyle);
+    this.btnStart = this.scene.add.text(0, -75, '', buttonStyle)
 
-    this.btnStart.setInteractive();
+    this.btnStart.setInteractive()
     this.btnStart.on('pointerdown', function (event) {
-      let deck: Card[] = deckRegion.deck.map( (cardImage) => cardImage.card)
+      let deck: Card[] = that.deck.map( (cardImage) => cardImage.card)
       this.scene.scene.start("GameScene", {deck: deck})
     })
     
-    this.updateStartButton();
+    this.updateStartButton()
     
-    this.container.add(this.btnStart);
+    this.container.add(this.btnStart)
   }
 
   addCard(card: Card): void {
     if (this.deck.length >= 15) {
-      this.scene.cameras.main.flash(300, 0, 0, 0.1);
-      return;
+      this.scene.cameras.main.flash(300, 0, 0, 0.1)
+      return
     }
 
-    let index = this.deck.length;
-    var image: Phaser.GameObjects.Image;
-    var [x, y] = this.getCardPosition(index);
+    let index = this.deck.length
+    var image: Phaser.GameObjects.Image
+    var [x, y] = this.getCardPosition(index)
     
-    image = this.scene.add.image(x, y, card.name);
-    image.setDisplaySize(100, 100);
+    image = this.scene.add.image(x, y, card.name)
+    image.setDisplaySize(100, 100)
 
-    image.setInteractive();
-    image.on('pointerdown', this.onClick(index), this);
+    image.setInteractive()
+    image.on('pointerdown', this.onClick(index), this)
 
-    this.container.add(image);
+    this.container.add(image)
 
-    this.deck.push(new CardImage(card, image));
+    this.deck.push(new CardImage(card, image))
 
-    this.updateStartButton();
+    this.updateStartButton()
   }
 
   private updateStartButton(): void {
     if (this.deck.length === 15) {
-      this.btnStart.text = 'Start';
-      this.btnStart.input.enabled = true;
+      this.btnStart.text = 'Start'
+      this.btnStart.input.enabled = true
     }
     else
     {
-      this.btnStart.text = `${this.deck.length}/15`;
-      this.btnStart.input.enabled = true; // TODO false
+      this.btnStart.text = `${this.deck.length}/15`
+      this.btnStart.input.enabled = true // TODO false
     }
   }
 
   private getCardPosition(index: number): [number, number] {
-    let xPad = space.pad;
-    let x = index * (space.cardSize - space.stackOverlap) + xPad + space.cardSize / 2;
+    let xPad = space.pad
+    let x = index * (space.cardSize - space.stackOverlap) + xPad + space.cardSize/2
 
-    let y = space.pad + space.cardSize / 2 + (index % 2) * space.stackOffset;
+    let y = space.pad + space.cardSize/2 + (index%2) * space.stackOffset
 
-    return [-x, -y];
+    return [-x, -y]
   }
 
   private onClick(index: number): () => void {
     return function() {
-      cardInfo.text = '';
+      // The text for the removed card would otherwise linger
+      cardInfo.text = ''
 
       // Remove the image
-      this.deck[index].destroy();
+      this.deck[index].destroy()
 
       // Remove from the deck array
-      this.deck.splice(index, 1);
+      this.deck.splice(index, 1)
 
-      this.correctDeckIndices();
+      this.correctDeckIndices()
 
-      this.updateStartButton();
+      this.updateStartButton()
     }
   }
 
-  // Set each card in deck to have the right position and onCLick events for its index
+  // Set each card in deck to have the right position and onClick events for its index
   private correctDeckIndices(): void {
     for (var i = 0; i < this.deck.length; i++) {
-      let image = this.deck[i].image;
+      let image = this.deck[i].image
 
-      image.setPosition(...this.getCardPosition(i));
+      image.setPosition(...this.getCardPosition(i))
 
-      // Send to back
       this.container.bringToTop(image)
 
       // Remove the previous onclick event and add one with the updated index
-      image.removeAllListeners('pointerdown');
-      image.on('pointerdown', this.onClick(i), this);
+      image.removeAllListeners('pointerdown')
+      image.on('pointerdown', this.onClick(i), this)
     }
   }
+
 
   private sort(): void {
     this.deck.sort(function (card1, card2): number {
       if (card1.card.cost < card2.card.cost)
       {
-        return -1;
+        return -1
       }
       else if (card1.card.cost > card2.card.cost)
       {
-        return 1;
+        return 1
       }
       else
       {
-        return card1.card.name.localeCompare(card2.card.name);
+        return card1.card.name.localeCompare(card2.card.name)
       }
-    });
+    })
 
-    this.correctDeckIndices();
+    this.correctDeckIndices()
   }
 }
 
@@ -289,23 +285,23 @@ class FilterRegion {
   scene: Phaser.Scene
   container: Phaser.GameObjects.Container
   catalogRegion
-  filter: boolean[] = []
+  filterCostAry: boolean[] = []
 
   constructor(scene: Phaser.Scene, catalogRegion) {
-    this.init(scene, catalogRegion);
+    this.init(scene, catalogRegion)
   }
 
   init(scene, catalogRegion): void {
     this.scene = scene
-    this.container = this.scene.add.container(1000, 20)
     this.catalogRegion = catalogRegion
+    this.container = this.scene.add.container(1000, 20)
   }
 
   create(): void {
     // Add each of the number buttons
     let btnNumbers: Phaser.GameObjects.Text[] = []
     for (var i = 0; i <= 8; i++) {
-      this.filter[i] = false
+      this.filterCostAry[i] = false
 
       let y = 50 * (i + 1)
       let btn = this.scene.add.text(30, y, i.toString(), filterButtonStyle)
@@ -329,7 +325,7 @@ class FilterRegion {
     let that = this
 
     return function() {
-        // Highlight the button, or remove its Highlight
+        // Highlight the button, or remove its highlight
         if (btn.isTinted) {
           btn.clearTint()
         }
@@ -339,17 +335,17 @@ class FilterRegion {
         }
 
         // Toggle filtering the chosen number
-        that.filter[i] = !that.filter[i]
+        that.filterCostAry[i] = !that.filterCostAry[i]
 
         // If nothing is filtered, all cards are shown
         let filterFunction
-        if (that.filter.every(v => v === false)) {
+        if (that.filterCostAry.every(v => v === false)) {
           filterFunction = function (card: Card) {return true}
         }
         else
         {
           filterFunction = function (card: Card) {
-            return that.filter[card.cost]
+            return that.filterCostAry[card.cost]
           }
         }
 
@@ -362,8 +358,8 @@ class FilterRegion {
     return function() {
       btns.forEach( (btn) => btn.clearTint())
 
-      for (var i = 0; i < that.filter.length; i++) {
-        that.filter[i] = false
+      for (var i = 0; i < that.filterCostAry.length; i++) {
+        that.filterCostAry[i] = false
       }
 
       that.catalogRegion.filter(
