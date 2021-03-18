e1f2a3a957975a5a215c6209c3d8dcded2edb105
Kai Geffen <geffenkai@gmail.com>
2021-03-18 18:11:31 -0400

    Added author names / urls to credits

--- src/welcomeScene.ts
+++ src/welcomeScene.ts
@@ -20,9 +20,20 @@ export class WelcomeScene extends Phaser.Scene {
     this.add.text(1100/2, 350, hintText,
       { font: '24px Arial Bold'}).setOrigin(0.5)
 
-    var creditText: string = "All icons from game-icons.net under CC BY 3.0"
-    this.add.text(1100/2, 600, creditText,
-      { font: '24px Arial Bold'}).setOrigin(0.5)
+    var creditText: string = "All icons are from game-icons.net under CC BY 3.0"
+    let creditsDelapouite = "Delapouite: https://delapouite.com/"//" {Sparkles, Spy, Swift, Crypt, Fishing Boat, Factory, Mine, Force, Uprising, Sarcophagus, Anubis, Horus, Enrage}"
+    let creditsLorc = "Lorc: https://lorcblog.blogspot.com/"//" {Bone Knife, Cog, Crossed Bones, Dove, Juggle, Drown, Gears, Dash, Cosmos, Fruiting, Gift, Paranoia, Hurricane, Dinosaur Bones, Oak}"
+    let creditsBerandas = "Lord Berandas: https://www.deviantart.com/berandas"// {Sine, AI}"
+    let creditsDarkZaitzev = "Dark Zaitzev: https://www.deviantart.com/darkzaitzev"// {Stalker}"
+    let creditsCathelineau = "Cathelineau"//: {Tumulus}"
+    
+    let authors = [creditsDelapouite, creditsLorc, creditsBerandas, creditsDarkZaitzev, creditsCathelineau]
+    for (var i = 0; i < authors.length; i++) {
+      creditText += '\n' + authors[i]
+    }
+
+    this.add.text(1100/2, 550, creditText,
+      { wordWrap: { width: 1000, useAdvancedWrap: true }}).setOrigin(0.5)
 
     this.input.on('pointerdown', function (/*pointer*/) {
       this.scene.start("BuilderScene")
