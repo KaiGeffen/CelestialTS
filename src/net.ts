e3f83412a8e045f751ad0ab2d623e406cc983be5
Kai Geffen <geffenkai@gmail.com>
2021-03-18 18:42:36 -0400

    Socket made relative to this uri

--- src/net.ts
+++ src/net.ts
@@ -10,7 +10,7 @@ const messageHeaders = {
 
 const bufSize = 4096 * 2
 
-const ip = '127.0.0.1'//'10.244.30.242'
+const ip = 'server'//'10.244.30.242'
 //'10.244.10.228'//'216.193.175.49'
 //'127.0.0.1'//'192.168.1.154' //'server-6d66b4ccc9-xc989'
 const port = 5555
@@ -28,7 +28,11 @@ export class Network {
 		})
 
 		// Create WebSocket connection.
-		let socket = new WebSocket(`ws://${ip}:${port}`, 'echo-protocol')
+		// let socket = new WebSocket(`ws://${ip}:${port}`, 'echo-protocol')
+		let loc = window.location
+		let fullPath = `ws://${loc.host}${loc.pathname}/ws`
+		let socket = new WebSocket(fullPath)
+
 		this.socket = socket
 
 		// Connection opened
