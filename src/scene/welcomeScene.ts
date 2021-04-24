bc15cb97c4490f12be07085cdac087c3a774370d
Kai Geffen <geffenkai@gmail.com>
2021-04-24 12:58:21 -0400

    Start button must be clicked (Can't click anywhere any more). Tutorial prompt for first time users added but disabled

--- src/scene/welcomeScene.ts
+++ src/scene/welcomeScene.ts
@@ -1,5 +1,5 @@
 import "phaser"
-import { StyleSettings, Space } from "../settings"
+import { StyleSettings, ColorSettings, Space } from "../settings"
 import { allCards } from "../catalog/catalog"
 import BaseScene from "./baseScene"
 import Button from "../lib/button"
@@ -64,16 +64,16 @@ export default class WelcomeScene extends BaseScene {
     this.add.text(Space.windowWidth/2, 200, "Celestial",
       StyleSettings.title).setOrigin(0.5)
 
-    let txtStart = this.add.text(Space.windowWidth/2, 350, "Click to start",
-      StyleSettings.announcement).setOrigin(0.5)
-
-    let backgroundClickable = this.add.rectangle(0, 0, Space.windowWidth, Space.windowHeight,
-      0x000, 0).setOrigin(0, 0)
-    backgroundClickable.setInteractive()
-    backgroundClickable.on('pointerdown', this.doStart, this)
+    // let txtStart = this.add.text(Space.windowWidth/2, 350, "Click to Start",
+    //   StyleSettings.announcement).setOrigin(0.5)
+    
+    // let backgroundClickable = this.add.rectangle(0, 0, Space.windowWidth, Space.windowHeight,
+    //   0x000, 0).setOrigin(0, 0)
+    // backgroundClickable.setInteractive()
+    // backgroundClickable.on('pointerdown', this.doStart, this)
 
     // Start Button
-    // let btnStart = new Button(this, Space.windowWidth/2, Space.windowHeight - 200, "Start", this.doStart).setOrigin(0.5)
+    new Button(this, Space.windowWidth/2, 350, "Click to Start", this.doStart).setOrigin(0.5).setStyle(StyleSettings.announcement)
 
     // Tutorial Button
     let btnTutorial = new Button(this, Space.windowWidth/2, Space.windowHeight - 125, "Tutorial", this.doTutorial).setOrigin(0.5)
@@ -82,6 +82,32 @@ export default class WelcomeScene extends BaseScene {
     let btnCredits = new Button(this, Space.windowWidth/2, Space.windowHeight - 50, "Credits", this.doCredits).setOrigin(0.5)
 
     super.create()
+
+    // Guide user to Tutorial if this is their first time here
+    // if (!JSON.parse(localStorage.getItem('tutorialComplete')) === true) {
+    //   this.createTutorialPrompt()
+    // }
+  }
+
+  private createTutorialPrompt(): void {
+    let promptContainer = this.add.container(0, 0).setDepth(25)
+    let exitPrompt = function() {
+      promptContainer.setVisible(false)
+    }
+
+    // Exit confirmation container
+    let invisibleBackground = this.add.rectangle(0, 0, Space.windowWidth, Space.windowHeight, 0xffffff, 0).setOrigin(0, 0)
+    invisibleBackground.setInteractive().on('pointerdown', exitPrompt, this)
+
+    let visibleBackground = this.add.rectangle(Space.windowWidth/2, Space.windowHeight/2, 800, 200, ColorSettings.menuBackground, 0.95)
+    visibleBackground.setInteractive()
+
+    let txtHint = this.add.text(Space.windowWidth/2, Space.windowHeight/2 - 40, 'Would you like to try the tutorial?', StyleSettings.announcement).setOrigin(0.5, 0.5)
+
+    let btnYes = new Button(this, Space.windowWidth/2 - 50, Space.windowHeight/2 + 40, 'Yes', this.doTutorial).setOrigin(1, 0.5)
+    let btnNo = new Button(this, Space.windowWidth/2 + 50, Space.windowHeight/2 + 40, 'No', exitPrompt).setOrigin(0, 0.5)
+
+    promptContainer.add([invisibleBackground, visibleBackground, txtHint, btnYes, btnNo])
   }
 
   private doStart(): void {
@@ -90,9 +116,9 @@ export default class WelcomeScene extends BaseScene {
   }
 
   private doTutorial(): void {
-    this.scene.start("TutorialScene1", {isTutorial: true, tutorialNumber: 1, deck: []})
+    // this.scene.start("TutorialScene1", {isTutorial: true, tutorialNumber: 1, deck: []})
 
-    // this.scene.start("BuilderScene", {isTutorial: true})
+    this.scene.start("BuilderScene", {isTutorial: true})
   }
 
   private doCredits(): void {
