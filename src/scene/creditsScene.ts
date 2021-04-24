0daaf11d78c7831910d64f9c4a3ad3387c9b9af9
Kai Geffen <geffenkai@gmail.com>
2021-04-24 15:22:33 -0400

    Added credits for 2nd page of cards, removed some cards from 2nd page

--- src/scene/creditsScene.ts
+++ src/scene/creditsScene.ts
@@ -26,13 +26,15 @@ export default class CreditsScene extends BaseScene {
     // Credit for all art
     let creditText: string = "All icons are from game-icons.net under CC BY 3.0"
 
-    let creditsDelapouite = "Delapouite: https://delapouite.com/\nSparkles, Spy, Swift, Crypt, Fishing Boat, Factory, Mine, Force, Uprising, Sarcophagus, Anubis, Horus, Enrage, Camera"
-    let creditsLorc = "Lorc: https://lorcblog.blogspot.com/\nBone Knife, Cog, Crossed Bones, Dove, Juggle, Drown, Gears, Dash, Cosmos, Fruiting, Gift, Paranoia, Hurricane, Dinosaur Bones, Oak, Cardback, Broken Bone, Robot"
+    let creditsDelapouite = "Delapouite: https://delapouite.com/\nSparkles, Spy, Swift, Crypt, Fishing Boat, Factory, Mine, Force, Uprising, Sarcophagus, Anubis, Horus, Enrage, Camera, Butterfly, Bandit, Wanted Poster, Night Vision, Bastet, Stable, Pelican, Beehive, Eagle, Icarus"
+    let creditsLorc = "Lorc: https://lorcblog.blogspot.com/\nBone Knife, Cog, Crossed Bones, Dove, Juggle, Drown, Gears, Dash, Cosmos, Fruiting, Gift, Paranoia, Hurricane, Dinosaur Bones, Oak, Cardback, Broken Bone, Robot, Bee, Disintegrate, Imprison"
     let creditsBerandas = "Lord Berandas: https://www.deviantart.com/berandas\nSine, AI"
     let creditsDarkZaitzev = "Dark Zaitzev: https://www.deviantart.com/darkzaitzev\nStalker"
-    let creditsCathelineau = "Cathelineau\nTumulus"
+    let creditsCathelineau = "Cathelineau: Tumulus"
+    let creditsSkoll = "Skoll: Raise Dead"
+    let creditsCaroAsercion = "Caro Asercion: Boar"
     
-    let authors = [creditsDelapouite, creditsLorc, creditsBerandas, creditsDarkZaitzev, creditsCathelineau]
+    let authors = [creditsDelapouite, creditsLorc, creditsBerandas, creditsDarkZaitzev, creditsCathelineau, creditsSkoll, creditsCaroAsercion]
     for (var i = 0; i < authors.length; i++) {
       creditText += '\n\n\t\t\t\t' + authors[i]
     }
