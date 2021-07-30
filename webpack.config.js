4b225cf4609bd3ac362a06f777ff2601b296dbb6
Kai Geffen <geffenkai@gmail.com>
2021-07-30 14:44:51 -0400

    Added cache-busting to webpack config using hashed time

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-const path = require('path');
+const path = require('path')
+const crypto = require('crypto')
+
+// Function to hash a string
+hashCode = s => s.split('').reduce((a,b)=>{a=((a<<5)-a)+b.charCodeAt(0);return a&a},0)
+
 module.exports = {
     entry: './src/app.ts',
     module: {
@@ -23,7 +28,7 @@ module.exports = {
         }
     },
     output: {
-        filename: 'app.js',
+        filename: 'app' + hashCode(Date.now.toString()) + '.js',
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'development'
