38e39811f59ae4345a6c7e455b86849a15bd3a3e
Kai Geffen <geffenkai@gmail.com>
2021-08-24 00:47:07 -0400

    Cards in builder are sorted first by cost, then expansion membership

--- src/catalog/catalog.ts
+++ src/catalog/catalog.ts
@@ -10,7 +10,17 @@ function dataToCards(_data: any[]): Card[] {
 }
 
 export const baseCards: Card[] = dataToCards(data)
+// Collectible cards are sorted by cost
 export const collectibleCards: Card[] = baseCards.concat(dataToCards(expansionData))
+.sort(function(a: Card, b: Card): number {
+  if (a.cost > b.cost) {
+    return 1
+  } else if (a.cost < b.cost) {
+    return -1
+  } else {
+    return 0
+  }
+})
 
 export const tokenCards: Card[] = dataToCards(tokenData)
 export const allCards: Card[] = collectibleCards.concat(tokenCards)
