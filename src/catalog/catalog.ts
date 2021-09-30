f04b56ce88005942776b89ff0ea9f09cb285a62a
Kai Geffen <geffenkai@gmail.com>
2021-09-29 23:31:28 -0400

    Added a json for cards that are only available locally but not on production

--- src/catalog/catalog.ts
+++ src/catalog/catalog.ts
@@ -1,6 +1,7 @@
 import data from "./catalog.json"
 import expansionData from "./catalogExpansion.json"
 import tokenData from "./tokens.json"
+import developmentData from "./catalogDevelopment.json"
 
 import Card from "../lib/card"
 
@@ -10,9 +11,18 @@ function dataToCards(_data: any[]): Card[] {
 }
 
 export const baseCards: Card[] = dataToCards(data)
+
+let availableCards
+if (location.port === '4949') {
+  availableCards = baseCards.concat(dataToCards(expansionData)).concat(dataToCards(developmentData))
+} else {
+  availableCards = baseCards.concat(dataToCards(expansionData))
+}
+
+
 // Collectible cards are sorted by cost
-export const collectibleCards: Card[] = baseCards.concat(dataToCards(expansionData))
-.sort(function(a: Card, b: Card): number {
+export const collectibleCards = availableCards
+  .sort(function(a: Card, b: Card): number {
   if (a.cost > b.cost) {
     return 1
   } else if (a.cost < b.cost) {
@@ -23,5 +33,7 @@ export const collectibleCards: Card[] = baseCards.concat(dataToCards(expansionDa
 })
 
 export const tokenCards: Card[] = dataToCards(tokenData)
-export const allCards: Card[] = collectibleCards.concat(tokenCards)
+
 export const cardback: Card = tokenCards[0]
+
+export const allCards: Card[] = collectibleCards.concat(tokenCards)
