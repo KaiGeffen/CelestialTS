2a0260165dea5d31f47380eefa5535b0dc01a985
Kai Geffen <geffenkai@gmail.com>
2021-10-13 18:00:44 -0400

    Updated the Rulebook, added an Overview

--- src/scene/baseScene.ts
+++ src/scene/baseScene.ts
@@ -241,6 +241,7 @@ export default class BaseScene extends Phaser.Scene {
 
 		let text = 
 `>>> SECTIONS
+Overview
 Start of match
 Start phase
 Action phase
@@ -249,6 +250,15 @@ Winning the match
 Drawing cards
 Precedence
 FAQ
+
+>>> OVERVIEW
+Celestial is a game in which 2 players compete to win 5 rounds before their opponent by playing cards face-down from their hand to the 'Story' in front of them.
+
+Once both players are done adding cards to the Story, all cards are revealed and their points are totaled. The player with the higher score wins that round.
+
+Cards have a variety of effects, such as: Revealing, creating, transforming, drawing, discarding, and removing from the game other cards. Use all of this to your advantage, and predict what your opponent is planning, in order to dominate at Celestial.
+
+Each player brings a deck of any 15 cards. If they would draw but their deck is empty, their discard pile is shuffled to form a new deck.
 
 >>> START OF MATCH
 Each player shuffles their 15 card deck.
@@ -290,9 +300,7 @@ Once a player has done either of the following, that player wins the match.
 
 >>> DRAWING CARDS
 When a player 'draws a card' they do the following:
-* If their hand has 6 cards in it, do nothing.
-* If their deck is empty, their discard pile is shuffled and becomes their deck.
-* They then take the top card of their deck and add it to their hand as the rightmost card.
+If they have 6 cards in hand, they skip their draw. Otherwise, they take the top card of their deck and add it to their hand as the rightmost card. If their deck is empty, their discard pile is shuffled to become their new deck.
 
 >>> PRECEDENCE
 Whenever a card would be selected from any zone (ex: Sarcophagus taking a card from your discard pile, or tutor drawing a card) the following system determines which card gets selected:
