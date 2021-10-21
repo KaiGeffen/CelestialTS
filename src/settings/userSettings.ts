8e91bd2f795550d83ce7c4476b9b54c8d829827f
Kai Geffen <geffenkai@gmail.com>
2021-10-21 16:38:08 -0400

    Added todo for sending data to server only as the window is closing

--- src/settings/userSettings.ts
+++ src/settings/userSettings.ts
@@ -68,6 +68,8 @@ export class UserSettings {
     if (key in sessionStorage) {
       sessionStorage.setItem(key, JSON.stringify(value))
 
+      // TODO Only update these on server when the window is closing, for efficiency
+
       // If key is in session storage then we're signed in
       // User progress and decks should be communicated to the server
       if (key === 'userProgress') {
