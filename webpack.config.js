67e9a14687b06d68d468ca34c17b5425008dc373
Kai Geffen <geffenkai@gmail.com>
2021-10-28 00:36:39 -0400

    Changing to production mode, breaking up individual module chunks to attempt performance improvement

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
             "Access-Control-Allow-Headers": "*"
         }
     },
-    mode: 'development',
+    mode: 'production',//'development',
     resolve: {
         extensions: [ '.ts', '.tsx', '.js' ],
         alias: {
@@ -31,24 +31,46 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    optimization: {
-     moduleIds: 'deterministic',
-      runtimeChunk: 'single',
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
+  //   optimization: {
+  //    moduleIds: 'deterministic',
+  //     runtimeChunk: 'single',
+  //     splitChunks: {
+  //       cacheGroups: {
+  //         vendor: {
+  //           test: /[\\/]node_modules[\\/]/,
+  //           name: 'vendors',
+  //           chunks: 'all',
+  //         },
+  //       },
+  //     },
+  // },
+  optimization: {
+  runtimeChunk: 'single',
+  splitChunks: {
+    chunks: 'all',
+    maxInitialRequests: Infinity,
+    minSize: 0,
+    cacheGroups: {
+      vendor: {
+        test: /[\\/]node_modules[\\/]/,
+        name(module) {
+          // get the name. E.g. node_modules/packageName/not/this/part.js
+          // or node_modules/packageName
+          const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+          // npm package names are URL-safe, but some servers don't like @ symbols
+          return `npm.${packageName.replace('@', '')}`;
         },
       },
+    },
   },
+},
     plugins: [
       new HtmlWebpackPlugin({
         title: 'Caching',
         template: 'template.html',
         filename: '../index.html',
-      }),
+      })
     ],
+
 };
