8b65dbf671b736b96f4d931ce0b2c7dca1c2ad72
Kai Geffen <geffenkai@gmail.com>
2022-06-05 16:43:26 -0400

    Hovering a card displays its cost, including extra mana, spent mana, how much more than you current mana it would cost

--- src/scene/matchRegions/ourScore.ts
+++ src/scene/matchRegions/ourScore.ts
@@ -2,7 +2,7 @@ import "phaser"
 
 import Region from './baseRegion'
 
-import { Space, Color, Style } from '../../settings/settings'
+import { Space, Color, Style, Mechanics } from '../../settings/settings'
 import Button from '../../lib/button'
 import { CardImage } from '../../lib/cardImage'
 import { cardback } from '../../catalog/catalog'
@@ -11,6 +11,10 @@ import BaseScene from '../baseScene'
 
 
 export default class ScoreRegion extends Region {
+	// For the current state, the maximum and current amount of breath we have
+	maxBreath: number
+	currentBreath: number
+
 	txtBreath: Phaser.GameObjects.Text
 	txtWins: Phaser.GameObjects.Text
 
@@ -28,16 +32,12 @@ export default class ScoreRegion extends Region {
 		// Create all of the breath icons
 		this.createBreathIcons()
 
-		// this.breathIcon = scene.add.sprite(30, height/2, 'icon-Breath').setOrigin(0, 0.5)
-		// this.costIcon = scene.add.sprite(30, height/2, 'icon-Cost').setOrigin(0, 0.5).setVisible(false)
 		const x = Space.windowWidth - 124
 		this.txtWins = scene.add.text(x, Space.windowHeight - 114, '', Style.basic).setOrigin(0)
 		this.txtBreath = scene.add.text(x + 6, Space.windowHeight - 60, '', Style.basic).setOrigin(0)
 		
-
 		// Add each of these objects to container
 		this.container.add([
-			// this.costIcon,
 			this.txtBreath,
 			this.txtWins,
 			])
@@ -46,9 +46,11 @@ export default class ScoreRegion extends Region {
 	}
 
 	displayState(state: ClientState, isRecap: boolean): void {
+		this.maxBreath = state.maxMana[0]
+		this.currentBreath = state.mana
+
 		// Reset the displayed cost
 		this.displayCost(0)
-		// this.breathIcon.setFrame(Math.min(10, state.mana))
 
 		const s = `${state.mana}/${state.maxMana[0]}`
 		this.txtBreath.setText(s)
@@ -58,18 +60,25 @@ export default class ScoreRegion extends Region {
 
 	// Display a given breath cost
 	displayCost(cost: number): void {
-		// this.costIcon.setVisible(cost > 0)
-		// this.costIcon.setFrame(Math.min(10, cost))
+		// Each is hidden by the one below
+		for (let i = 0; i < 10; i++) {
+			this.breathSpent[i].setVisible(i < this.maxBreath)
+			this.breathExtra[i].setVisible(i < this.currentBreath)
+			this.breathBasic[i].setVisible(i < Math.min(this.maxBreath, this.currentBreath))
+			this.breathOom[i].setVisible(i < cost)
+			this.breathHover[i].setVisible(i < Math.min(cost, this.currentBreath))
+		}
 	}
 
 	// Create all of the breath icons
 	private createBreathIcons(): void {
+		// NOTE Order matters, earliest is on the bottom
 		const breathMap = {
-			'Basic': this.breathBasic,
 			'Spent': this.breathSpent,
 			'Extra': this.breathExtra,
-			'Hover': this.breathHover,
+			'Basic': this.breathBasic,
 			'Oom': this.breathOom,
+			'Hover': this.breathHover,
 		}
 
 		for (let key in breathMap) {
@@ -82,16 +91,16 @@ export default class ScoreRegion extends Region {
 		const center = [Space.windowWidth - 163, Space.windowHeight - 53]
 		const radius = 30
 
-		// TODO 10 max breath displayed
-		const MAX = 10
-		for (let i = 0; i < MAX; i++) {
+		// 10 is the max displayed breath, but player could have more
+		for (let i = 0; i < 10; i++) {
 			// Angle in radians
-			const theta = -2 * Math.PI * i / MAX
+			const theta = 2 * Math.PI * i / 10
 
 			const x = center[0] + Math.cos(theta) * radius
 			const y = center[1] + Math.sin(theta) * radius
 			const s = `icon-Breath${key}`
 
+			// Create the icon, add it to container and list of breath for this subtype
 			let image = this.scene.add.image(x, y, s)
 			this.container.add(image)
 			images.push(image)
