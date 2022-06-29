d802629a39ad998820c4485e6e8e1cc5f5d24e4d
Kai Geffen <geffenkai@gmail.com>
2022-06-29 18:47:52 -0400

    Removed unused animation code from discard piles region

--- src/scene/matchRegions/discardPiles.ts
+++ src/scene/matchRegions/discardPiles.ts
@@ -26,114 +26,24 @@ export default class DiscardPilesRegion extends Region {
 		let that = this
 
 		// Ours
-		let ourDiscard = []
 		for (let i = 0; i < state.discard[0].length; i++) {
 			let card = this.addCard(state.discard[0][i], CardLocation.ourDiscard(this.container, i))
-			
-			card.setOnClick(that.ourCallback)
+			.setOnClick(that.ourCallback)
 
 			this.temp.push(card)
-			ourDiscard.push(card)
 		}
-		this.animate(state, ourDiscard, 0, isRecap)
 
 		// Theirs
-		let theirDiscard = []
 		for (let i = 0; i < state.discard[1].length; i++) {
 			let card = this.addCard(state.discard[1][i], CardLocation.theirDiscard(this.container, i))
-
-			card.setOnClick(that.theirCallback)
+			.setOnClick(that.theirCallback)
 
 			this.temp.push(card)
-			theirDiscard.push(card)
 		}
-		this.animate(state, theirDiscard, 1, isRecap)
 	}
 
 	setCallback(ourCallback: () => void, theirCallback: () => void): void {
 		this.ourCallback = ourCallback
 		this.theirCallback = theirCallback
-	}
-
-	// TODO Go backwards so that cards are hidden that haven't yet reached the discard pile
-	// Animate any cards ending in the hand
-	private animate(state: ClientState, cards: CardImage[], player: number, isRecap: boolean): void {
-		return
-		
-		let scene = this.scene
-		
-		// Keep a count of how many cards have gone to the discard already
-		// in order to hide the right card
-		let count = 0
-
-		for (let i = state.animations[player].length - 1; i >= 0; i--) {
-			let delay = i * Time.recapTween()
-
-			let animation = state.animations[player][i]
-			if (animation.to === Zone.Discard) {
-				// Create an image for the card in flight, hide the real copy until animation has completed
-				let card = this.addCard(animation.card, 
-					player === 0 ? CardLocation.ourDiscard(this.container) : CardLocation.theirDiscard(this.container)
-					)
-
-				// Animate the card coming from given zone
-				// Remember where to end, then move to starting position
-				let x = card.container.x
-				let y = card.container.y
-
-				if (animation.from === Zone.Discard) {
-					// This is the card having an effect in the player's discard pile
-					this.animateEmphasis(card, delay)
-				}
-				else {
-					// Set the starting position based on zone it's coming from
-					let position
-					switch (animation.from) {
-						case Zone.Hand:
-						position = player === 0 ? CardLocation.ourHand(undefined, 0, this.container) : CardLocation.theirHand(undefined, 0, this.container)
-						break
-
-						case Zone.Deck:
-						position = player === 0 ? CardLocation.ourDeck(this.container) : CardLocation.theirDeck(this.container)
-						break
-
-						case Zone.Story:
-						position = CardLocation.story(state, isRecap, animation.index, this.container, player)
-						break
-
-						case Zone.Gone:
-						position = CardLocation.gone(this.container)
-						break
-					}
-					card.setPosition(position)
-
-					// Hide the card until it starts animating
-					card.hide()
-
-					// TODO Has a bug if discard is shuffled in this action (Parch)
-					// Hide the existing card in the discard pile
-					count += 1
-					// cards[cards.length - count].hide()
-
-					// Animate moving x direction, becoming visible when animation starts
-					this.scene.tweens.add({
-						targets: card.container,
-						x: x,
-						y: y,
-						delay: delay,
-						duration: Time.recapTweenWithPause(),
-						onStart: function (tween, targets, _)
-						{
-							card.show()
-							scene.sound.play('discard')
-						},
-						onComplete: () => {
-							// cards[cards.length - count].show()
-							card.destroy()
-						}
-					})
-				}
-			}
-		}
 	}
 }
