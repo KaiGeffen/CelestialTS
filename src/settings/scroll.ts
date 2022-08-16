ad4cc1148d73c503ef44ee9629c6b6c08e345818
Kai Geffen <geffenkai@gmail.com>
2022-08-16 11:57:46 -0400

    Made scroll track flat instead of slightly rounded

--- src/settings/scroll.ts
+++ src/settings/scroll.ts
@@ -2,11 +2,13 @@ import 'phaser'
 import { Color, Space } from './settings'
 
 
+const ROUNDING = Space.scrollWidth/2
+
 export const Scroll: (scene: Phaser.Scene) => any =
 (scene: Phaser.Scene) => {
 	return {
 		input: 'click',
-		track: scene['rexUI'].add.roundRectangle(0, 0, Space.scrollWidth, 0, 5, Color.sliderTrack),
-		thumb: scene['rexUI'].add.roundRectangle(0, 0, 0, 0, Space.scrollWidth/2, Color.sliderThumb),
+		track: scene['rexUI'].add.roundRectangle(0, 0, Space.scrollWidth, 0, 0, Color.sliderTrack),
+		thumb: scene['rexUI'].add.roundRectangle(0, 0, 0, 0, ROUNDING, Color.sliderThumb),
 	}
 }
