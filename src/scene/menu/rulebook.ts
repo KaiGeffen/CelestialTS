3c05bfc785db94297c66c234884778aeb7fb9684
Kai Geffen <geffenkai@gmail.com>
2022-08-26 17:50:51 -0400

    Updated the rulebook to be consistent with most recent rules

--- src/scene/menu/rulebook.ts
+++ src/scene/menu/rulebook.ts
@@ -90,7 +90,7 @@ Celestial is a game in which 2 players compete to win 5 rounds before their oppo
 
 Once both players are done adding cards to the Story, all cards are revealed and their points are totaled. The player with the higher score wins that round.
 
-Cards have a variety of effects, such as: Revealing, creating, transforming, drawing, discarding, and removing from the game other cards. Use all of this to your advantage, and predict what your opponent is planning, in order to dominate at Celestial.
+Cards have a variety of effects, such as: Revealing, creating, transforming, drawing, discarding, and removing from the game other cards. Use all of this to your advantage, and predict what your opponent is planning, in order to win at Celestial.
 
 Each player brings a deck of any 15 cards. If they would draw but their deck is empty, their discard pile is shuffled to form a new deck.
 
@@ -108,14 +108,14 @@ Each round has the following structure: start phase, action phase, end phase.
 In the start phase, the following things occur in the following order:
 * Any 'start of round' effects trigger (ex: Sun).
 * If one player has won more rounds than the other, that player receives priority. Otherwise, priority is determined at random.
-* Each player's maximum mana increases by 1 if it is less than 10.
-* Each player's current mana is set to their maximum mana.
+* Each player's maximum breath increases by 1 if it is less than 10.
+* Each player's current breath is set to their maximum breath.
 * Each player draws 2 cards.
 
 >>> ACTION PHASE
-During the action phase, the player with priority can either pass, or play a card from their hand (Assuming they have sufficient mana to pay for it).
+During the action phase, the player with priority can either pass, or play a card from their hand (Assuming they have sufficient breath to pay for it).
 If they pass, their opponent is given priority.
-If they play a card, they pay mana from their current mana equal to that card's cost.
+If they play a card, they pay breath from their current breath equal to that card's cost.
 The card then moves onto the story as the rightmost addition.
 At this time, any 'when played' effects of the card activate (ex: Night Vision).
 Their opponent is then given priority.
@@ -125,19 +125,17 @@ During this phase, each player cannot see the cards their opponent has played.
 >>> END PHASE
 During the end phase, cards in the story resolve from left to right.
 When a card resolves, it adds its points to its owner's score for the round, then its effect occurs, then it moves to its owner's discard pile.
-Once all cards in the story have resolved, if a player has a positive score that is greater than their opponent's, they are awarded a round win.
+Once all cards in the story have resolved, if on player has more points than their opponent, they are awarded a round win.
 
 >>> WINNING THE MATCH
-Once a player has done either of the following, that player wins the match.
-* Win at least 5 rounds, and have won at least 2 more rounds than their opponent.
-* Win 10 rounds.
+Once a player has won 5 rounds, they win the match.
 
 >>> DRAWING CARDS
 When a player 'draws a card' they do the following:
 If they have 6 cards in hand, they skip their draw. Otherwise, they take the top card of their deck and add it to their hand as the rightmost card. If their deck is empty, their discard pile is shuffled to become their new deck.
 
 >>> PRECEDENCE
-Whenever a card would be selected from any zone (ex: Sarcophagus taking a card from your discard pile, or tutor drawing a card) the following system determines which card gets selected:
+Whenever a card would be selected from any zone (ex: Cling taking the highest cost card from your discard pile) the following system determines which card gets selected:
 * First the deck is traversed from top to bottom, and any card meeting the conditions is picked.
 * Then the discard pile is traversed from top to bottom, and any card meeting the conditions is picked.
 * If no cards are picked this way, the effect does nothing.
@@ -147,10 +145,4 @@ Is my deck in the order that I see when hovering over it?
 No, the true order of your deck is hidden from you. The order you see is sorted by cost.
 
 Can cards that reset (ex: Hurricane) be worth points if they are Nourished?
-No, the card contributes points first, then its effect resets your points to 0.
-
-How is the Inspire trait different than Inspired?
-Cards give you Inspire the round that they resolve, which in the Start Phase changes to that much Inspired and reflects the extra mana you have gained for that round.
-
-Do Nourish and Starve cancel each other out?
-They do not; you can have both Nourish and Starve at the same time.`
+No, the card contributes points first, then its effect resets your points to 0.`
