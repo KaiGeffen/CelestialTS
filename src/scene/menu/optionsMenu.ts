3bcd021e94db9fcd22bb310c074231239d992509
Kai Geffen <geffenkai@gmail.com>
2023-01-29 18:05:18 -0500

    Began separating options menu into tabs

--- src/scene/menu/optionsMenu.ts
+++ src/scene/menu/optionsMenu.ts
@@ -12,7 +12,9 @@ import Buttons from '../../lib/buttons/buttons'
 import MenuScene from '../menuScene'
 
 
-const width = 550
+const width = 650
+// Width of the subpanel that shows selected tab's contents
+const subWidth = 400
 
 export default class OptionsMenu extends Menu {
 	constructor(scene: MenuScene, params) {
@@ -25,29 +27,102 @@ export default class OptionsMenu extends Menu {
 		this.layout()
 	}
 
-	onClose(): void {
-		
-	}
-
 	private createContent(activeScene: BaseScene) {
 		this.createHeader('Options', width + Space.padSmall*2)
 
-		const padding = {padding: {left: Space.padSmall, right: Space.padSmall}}
+		// Sizer with tabs on left, contents on right
+		let subSizer = this.scene['rexUI'].add.sizer({
+			space: {
+				left: Space.pad,
+				right: Space.pad,
+			}
+		})
+		this.sizer.add(subSizer)
 
-		this.sizer.add(this.createVolume(), padding)
-		.addNewLine()
+		// Create the different tabs that user can select
+		let tabs = this.createTabs()
+		subSizer.add(tabs)
+		.addSpace()
 
-		this.sizer.add(this.createMusic(), padding)
-		.addNewLine()
+		// Create a sizer for each of the tabs
+		// this.create()
 
-		this.sizer.add(this.createSpeed(), padding)
-		.addNewLine()
-		
-		this.sizer.add(this.createAutopass(), padding)
-		.addNewLine()
-
-		this.sizer.add(this.createButtons(activeScene), padding)
+		// Put the currently selected tab's contents in the main sizer
+		const view = this.createGeneralContents(activeScene)
+		subSizer.add(view)
 	}
+
+	private createTabs()  {
+		let tabsSizer = this.scene['rexUI'].add.fixWidthSizer({space: {line: Space.pad}})
+
+		const tabStrings = ['General', 'Audio', 'Rulebook', 'Credits']
+
+		tabsSizer.addNewLine()
+		for (let i = 0; i < tabStrings.length; i++) {
+			let container = new ContainerLite(this.scene, 0, 0, Space.largeButtonWidth, Space.largeButtonHeight)
+			let btn = new Buttons.Basic(container, 0, 0, tabStrings[i])
+			.setOnClick(() => {
+				// TODO switch to right tab and remember
+			})
+
+			tabsSizer.add(container)
+			.addNewLine()
+		}
+
+		return tabsSizer
+	}
+
+	private createGeneralContents(activeScene: BaseScene) {
+		let sizer = this.scene['rexUI'].add.fixWidthSizer({space: {
+			line: Space.pad*2,
+			top: Space.pad,
+			bottom: Space.pad,
+			left: Space.pad,
+			right: Space.pad,
+		}})
+		.addBackground(this.scene.add.rectangle(0, 0, 1, 1, Color.background2))
+
+		sizer
+		.add(this.createAutopass())
+		.addNewLine()
+		.add(this.createSpeed())
+		.addNewLine()
+		.add(this.createQuit(activeScene))
+
+		return sizer
+	}
+
+	private createAudioContents() {
+		let sizer = this.scene['rexUI'].add.fixWidthSizer({space: {line: Space.pad}})
+
+		sizer
+		.add(this.createVolume())
+		.addNewLine()
+		.add(this.createMusic())
+		// .addNewLine()
+		// .add(this.createDialog())
+
+		return sizer
+	}
+
+	// 	// Right side content
+
+	// 	const padding = {padding: {left: Space.padSmall, right: Space.padSmall}}
+
+	// 	this.sizer.add(this.createVolume(), padding)
+	// 	.addNewLine()
+
+	// 	this.sizer.add(this.createMusic(), padding)
+	// 	.addNewLine()
+
+	// 	this.sizer.add(this.createSpeed(), padding)
+	// 	.addNewLine()
+		
+	// 	this.sizer.add(this.createAutopass(), padding)
+	// 	.addNewLine()
+
+	// 	this.sizer.add(this.createButtons(activeScene), padding)
+	// }
 
 	private createVolume() {
 		let that = this
@@ -117,7 +192,7 @@ export default class OptionsMenu extends Menu {
 	}
 
 	private createSpeed() {
-		let sizer = this.scene['rexUI'].add.sizer({width: width})
+		let sizer = this.scene['rexUI'].add.sizer({width: subWidth})
 
 		let txtSpeedHint = this.scene.add.text(0, 0, 'Speed:', Style.basic)
 		sizer.add(txtSpeedHint)
@@ -147,14 +222,14 @@ export default class OptionsMenu extends Menu {
 	}
 
 	private createAutopass() {
-		let sizer = this.scene['rexUI'].add.sizer({width: width})
+		let sizer = this.scene['rexUI'].add.sizer({width: subWidth})
 
 		let txtVolumeHint = this.scene.add.text(0, 0, 'Autopass:', Style.basic)
 		sizer.add(txtVolumeHint)
 		sizer.addSpace()
 
 		const s = UserSettings._get('autopass') ? 'Enabled' : 'Disabled'
-		let container = new ContainerLite(this.scene, 0, 0, Space.smallButtonWidth, Space.smallButtonHeight)
+		let container = new ContainerLite(this.scene, 0, 0, Space.largeButtonWidth, Space.largeButtonHeight)
 		let btn = new Buttons.Basic(container, 0, 0, s, () => {
 			if (UserSettings._get('autopass')) {
 				btn.setText('Disabled')
@@ -190,7 +265,7 @@ export default class OptionsMenu extends Menu {
 	}
 
 	private createReadRulebook() {
-		let container = new ContainerLite(this.scene, 0, 0, Space.smallButtonWidth, 50)
+		let container = new ContainerLite(this.scene, 0, 0, Space.largeButtonWidth, 50)
 
 		new Buttons.Basic(container, 0, 0, 'Rulebook', () => {
 			this.scene.scene.start('MenuScene', {menu: 'rulebook'})
@@ -200,7 +275,7 @@ export default class OptionsMenu extends Menu {
 	}
 
 	private createCredits() {
-		let container = new ContainerLite(this.scene, 0, 0, Space.smallButtonWidth, 50)
+		let container = new ContainerLite(this.scene, 0, 0, Space.largeButtonWidth, 50)
 
 		new Buttons.Basic(container, 0, 0, 'Credits', () => {
 			this.scene.scene.start('MenuScene', {menu: 'credits'})
@@ -210,7 +285,16 @@ export default class OptionsMenu extends Menu {
 	}
 
 	private createQuit(activeScene: BaseScene) {
-		let container = new ContainerLite(this.scene, 0, 0, Space.smallButtonWidth, 50)
+		let sizer = this.scene['rexUI'].add.sizer({
+			width: subWidth,
+			space: {item: Space.pad},
+		})
+
+		let container = new ContainerLite(this.scene, 0, 0, Space.largeButtonWidth, 50)
+		sizer
+		.addSpace()
+		.add(container)
+		.addSpace()
 
 		new Buttons.Basic(container, 0, 0, 'Quit', () => {
 			// Stop the other active scene
@@ -221,6 +305,6 @@ export default class OptionsMenu extends Menu {
 			this.scene.scene.start("HomeScene")
 		})
 
-		return container
+		return sizer
 	}
 }
