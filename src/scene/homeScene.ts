22caede8ce6ac33c6198c3f272802e7730e50456
Kai Geffen <geffenkai@gmail.com>
2023-02-06 16:54:06 -0500

    Fixed bug where session data for logged in user persists when reloading page

--- src/scene/homeScene.ts
+++ src/scene/homeScene.ts
@@ -53,8 +53,6 @@ export default class HomeScene extends BaseScene {
         this.scene.launch('MenuScene', {
           menu: 'confirm',
           callback: () => {
-            Server.logout()
-            
             this.scene.start('SigninScene')
           },
           hint: 'logout'
--- src/scene/preloadScene.ts
+++ src/scene/preloadScene.ts
@@ -22,6 +22,9 @@ export class SigninScene extends Phaser.Scene {
 	create(): void {
 		document.getElementById("signin").hidden = false
 
+		// Ensure user is signed out
+		Server.logout()
+
 		// Add buttons to sign in or play as a guest
 		this.createButtons()
 	}
