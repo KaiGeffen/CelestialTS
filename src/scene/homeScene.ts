bd3379887e0e107311bbb7197e917ab5bca93421
Kai Geffen <geffenkai@gmail.com>
2023-02-10 22:10:01 -0500

    Changed the Free Play animation to have avatars + cards

--- src/scene/homeScene.ts
+++ src/scene/homeScene.ts
@@ -1,5 +1,5 @@
 import "phaser"
-import { Style, Color, Space, UserProgress, Url, UserSettings } from "../settings/settings"
+import { Style, Color, Space, Time, Ease, UserProgress, Url, UserSettings } from "../settings/settings"
 import { allCards } from "../catalog/catalog"
 import BaseScene from "./baseScene"
 import Button from "../lib/buttons/button"
@@ -10,6 +10,8 @@ import Menu from "../lib/menu"
 import intro from "../adventures/intro.json"
 import Loader from '../loader/loader'
 import Server from '../server'
+import { CardImage } from '../lib/cardImage'
+import { baseCards } from "../catalog/catalog"
 
 
 const headerHeight = Space.iconSize + Space.pad * 2
@@ -83,11 +85,16 @@ export default class HomeScene extends BaseScene {
   }
 
   private createButtons(): void {
-    const y = headerHeight + (Space.windowHeight - headerHeight)/2
+    // const y = headerHeight + (Space.windowHeight - headerHeight)/2
 
     const width = (Space.windowWidth - Space.pad * 3)/2
     const height = Space.windowHeight - headerHeight - Space.pad * 2
 
+    this.createAdventureButton(width, height)
+    this.createDeckbuilderButton(width, height)
+  }
+
+  private createAdventureButton(width: number, height: number): void {
     let rectLeft = this.add.rectangle(Space.windowWidth/2 - Space.pad/2,
       headerHeight + Space.pad,
       width,
@@ -139,39 +146,123 @@ export default class HomeScene extends BaseScene {
     // Text over the rectangle
     this.add.text(rectLeft.x - rectLeft.displayWidth/2, rectLeft.y + rectLeft.displayHeight/2, 'Adventure', Style.homeTitle)
     .setOrigin(0.5)
+  }
 
-
+  private createDeckbuilderButton(width: number, height: number): void {
+    const x = Space.windowWidth/2 + Space.pad/2
+    const y = headerHeight + Space.pad
 
     // Free Play button
-    let rectRight = this.add.rectangle(Space.windowWidth/2 + Space.pad/2,
-      headerHeight + Space.pad,
+    let rectRight = this.add.rectangle(x,
+      y,
       width,
       height,
-      0x303030,
+      0xE9DFCE,
       1)
-    .setOrigin(0, 0)
-
-    let freePlay = this.add.sprite(
-      rectRight.x,
-      rectRight.y,
-      'bg-Free Play')
     .setOrigin(0)
 
+    // Container with visual elements of the button
+    let container = this.add.container(x, y)
+
+    // Character avatars
+    let avatar1 = this.add.sprite(width/2, 0, 'avatar-JulesFull')
+    .setOrigin(1, 0)
+    let avatar2 = this.add.sprite(width/2, Space.cardHeight, 'avatar-MiaFull')
+    .setOrigin(0)
+    container.add([avatar2, avatar1])
+
+    for (let i = 0; i < 3; i++) {
+      // Card in their hand
+      const x1 = width - ((2 - i) * Space.stackOverlap * 2)
+      this.addCard(container, x1, 0, i)
+      
+      // Card in our hand
+      const x2 = i * Space.stackOverlap * 2
+      this.addCard(container, x2, height, i)
+    }
+    
     // While not hovered, rectangle is greyed
     rectRight.setInteractive()
     .on('pointerover', () => {
-      freePlay.setTint(0x444444)
+      container.iterate((child) => {
+        child.setTint(0x444444)
+      })
     })
     .on('pointerout', () => {
-      freePlay.clearTint()
+      container.iterate((child) => {
+        child.clearTint()
+      })
     })
     .on('pointerdown', this.doStart())
 
-    freePlay.mask = new Phaser.Display.Masks.BitmapMask(this, rectRight)
+    container.mask = new Phaser.Display.Masks.BitmapMask(this, rectRight)
 
     // Text over the rectangle
     this.add.text(rectRight.x + rectRight.displayWidth/2, rectRight.y + rectRight.displayHeight/2, 'Free Play', Style.homeTitle)
     .setOrigin(0.5)
+  }
+
+  private addCard(container: Phaser.GameObjects.Container, x: number,y: number, delay: number): void {
+    // Becomes a random card when the tween starts
+    const card = baseCards[0].name
+    const top = y === 0
+
+    const imgX = top ? x + 500 : x - 500
+    let img = this.add.image(imgX, y, card)
+    .setOrigin(top ? 1 : 0, top ? 0 : 1)
+    container.add(img)
+
+    // Tween
+    const hold = 6000
+    const repeatDelay = Time.recapTweenWithPause() * 3
+    this.tweens.add({
+      targets: img,
+      x: x,
+      delay: Time.recapTweenWithPause() * delay,
+      repeat: -1,
+      duration: Time.recapTween(),
+      hold: hold,
+      repeatDelay: repeatDelay,
+      ease: Ease.basic,
+      onStart: () => {
+        const cardNum = Math.floor(Math.random() * (baseCards.length - 1))
+        const card = baseCards[cardNum].name
+        img.setTexture(card)
+
+        // When holding completes, tween the card dropping offscreen
+        this.tweens.add({
+          targets: img,
+          y: y + Space.cardHeight,
+          delay: hold,
+          duration: repeatDelay,
+          ease: Ease.basic,
+          onComplete: () => {
+            // Reset the y
+            img.setY(y)
+          }
+        })
+      },
+      
+      onRepeat: () => {
+        const cardNum = Math.floor(Math.random() * (baseCards.length - 1))
+        const card = baseCards[cardNum].name
+        img.setTexture(card)
+
+        // TODO Not dry with the above identical code
+        // When holding completes, tween the card dropping offscreen
+        this.tweens.add({
+          targets: img,
+          y: y + Space.cardHeight,
+          delay: hold,
+          duration: repeatDelay,
+          ease: Ease.basic,
+          onComplete: () => {
+            // Reset the y
+            img.setY(y)
+          }
+        })
+      },
+    })
   }
 
   private displayMessage(message: string): void {
