b347e115ed46802f8cbb31576d5b5df1e3e00363
Kai Geffen <geffenkai@gmail.com>
2023-05-05 15:35:50 -0400

    Round result animation includes rendered text. Removed text

--- src/scene/matchRegions/roundResult.ts
+++ src/scene/matchRegions/roundResult.ts
@@ -9,7 +9,6 @@ import Region from './baseRegion';
 // Shows the current scores of the night's performance
 // As well as any buttons
 export default class RoundResultRegion extends Region {
-	txtRoundResult: Phaser.GameObjects.Text
 	roundResult: Phaser.GameObjects.Sprite
 
 	create (scene: BaseScene): RoundResultRegion {
@@ -19,18 +18,8 @@ export default class RoundResultRegion extends Region {
 		// Image in the center saying if you won/lost/tied
 		this.roundResult = scene.add.sprite(Space.windowWidth/2, Space.windowHeight/2, 'icon-RoundWin', 2)
 		.setAlpha(0)
-
-		this.txtRoundResult = scene.add.text(
-			this.roundResult.getRightCenter().x - 60,
-			this.roundResult.getRightCenter().y,
-			'',
-			Style.roundResult)
-		.setOrigin(1, 0.5)
 		
-		this.container.add([
-			this.roundResult,
-			this.txtRoundResult
-			])
+		this.container.add(this.roundResult)
 
 		return this
 	}
@@ -63,12 +52,9 @@ export default class RoundResultRegion extends Region {
 		this.roundResult.setTexture(name, 0)
 		.play(name)
 
-		// Set the text
-		this.txtRoundResult.setText(s)
-
 		// Tween it fading in and out
 		this.scene.tweens.add({
-			targets: [this.roundResult, this.txtRoundResult],
+			targets: this.roundResult,
 			duration: 200,
 			hold: 2000,
 			ease: "Sine.easeInOut",
@@ -76,11 +62,9 @@ export default class RoundResultRegion extends Region {
 			yoyo: true,
 			onStart: () => {
 				this.roundResult.setAlpha(0)
-				this.txtRoundResult.setAlpha(0)
 			},
 			onComplete: () => {
 				this.roundResult.setAlpha(0)
-				this.txtRoundResult.setAlpha(0)
 			},
 		})
 	}
