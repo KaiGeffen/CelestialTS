9874bda41a98e3a93ec022a0c78dba1a33b6b9cf
Kai Geffen <geffenkai@gmail.com>
2023-12-27 04:37:13 -0500

    Hint is now a text object, doesnt work with images

--- src/lib/hint.ts
+++ src/lib/hint.ts
@@ -9,7 +9,9 @@ import { Keyword, keywords } from '../catalog/keywords'
 
 
 export default class Hint {
+	scene: Phaser.Scene
 	txt: RexUIPlugin.BBCodeText
+	element: HTMLElement
 
 	// The X position to position flush to, or undefined if no pin
 	leftPin: number
@@ -19,17 +21,14 @@ export default class Hint {
 	skipWait = false
 
 	constructor(scene: Phaser.Scene) {
-		this.txt = scene['rexUI'].add.BBCodeText(Space.windowWidth/2, Space.windowHeight/2, 'Hello world', BBStyle.hint)
-		.setOrigin(0.5, 1)
-		.setDepth(40)
-		.setVisible(false)
-		.setAlign('center')
+		this.scene = scene
+		this.element = document.getElementById('hint')
 
 		// Copy mouse position and show a hint when over a hinted object
 		scene.input.on('pointermove', () => {
 			this.orientText()
 			if (!this.skipWait) {
-				this.txt.setAlpha(0)
+				this.element.style.opacity = "0"
 				this.waitTime = 0
 			}
 		})
@@ -38,13 +37,13 @@ export default class Hint {
 				this.waitTime += delta
 			}
 			else {
-				this.txt.setAlpha(1)
+				this.element.style.opacity = "1"
 			}
 		})
 	}
 
 	hide(): Hint {
-		this.txt.setVisible(false)
+		this.element.style.opacity = "0"
 
 		// Reset the pin, since the next hovered item might not pin
 		this.leftPin = undefined
@@ -54,7 +53,7 @@ export default class Hint {
 
 	show(): Hint {
 		this.orientText()
-		this.txt.setVisible(true)
+		this.element.style.opacity = "1"
 
 		return this
 	}
@@ -68,28 +67,28 @@ export default class Hint {
 		}
 		
 		let hintText = card.getHintText()
-		const referencedImages = card.getReferencedCards().map((card) => {
-			return ` [img=${card.name}]`
-		}).join()
+		// const referencedImages = card.getReferencedCards().map((card) => {
+		// 	return ` [img=${card.name}]`
+		// }).join()
 		if (hintText !== '') {
 			this.showText(hintText)
 
 			// NOTE This is a hack because of a bug where card image renders with a single line's height
-			this.txt
-			.setText(`[img=${card.name}]`)
-			.appendText(`[color=grey]${referencedImages}[/color]`)
-			.appendText('\n\n\n\n\n\n\n\n\n\n\n\n')
-			.appendText(`\n${hintText}`)
-			.setFixedSize(0, 0)
+			// this.txt
+			// .setText(`[img=${card.name}]`)
+			// .appendText(`[color=grey]${referencedImages}[/color]`)
+			// .appendText('\n\n\n\n\n\n\n\n\n\n\n\n')
+			// .appendText(`\n${hintText}`)
+			// .setFixedSize(0, 0)
 		}
 		else {
-			const width = card.getReferencedCards().length > 0 ? Space.maxTextWidth + Space.pad : Space.cardWidth + Space.pad
-			this.txt.setText(`[img=${card.name}]`)
-			.appendText(`${referencedImages}`)
-			.setFixedSize(
-				width,
-				Space.cardHeight + Space.pad
-				)
+			// const width = card.getReferencedCards().length > 0 ? Space.maxTextWidth + Space.pad : Space.cardWidth + Space.pad
+			// this.txt.setText(`[img=${card.name}]`)
+			// .appendText(`${referencedImages}`)
+			// .setFixedSize(
+			// 	width,
+			// 	Space.cardHeight + Space.pad
+			// 	)
 		}
 
 		return this
@@ -100,8 +99,7 @@ export default class Hint {
 			this.show()
 		}
 
-		this.txt.setText(s)
-		.setFixedSize(0, 0)
+		this.element.textContent = s
 	}
 
 	// TODO Use in more places, instead of forming a string then passing to showText
@@ -130,20 +128,29 @@ export default class Hint {
 
 	// Orient the text to be in the right position relative to the mouse
 	private orientText(): void {
-		const pointer = this.txt.scene.game.input.activePointer
+		const pointer = this.scene.game.input.activePointer
 
 		// Unless there is a left pin, center and hover above the mouse position
 		if (this.leftPin === undefined) {
-			this.txt.setX(pointer.position.x)
-			.setOrigin(0.5, 1)
-			.setY(pointer.position.y - Space.pad)
+			this.element.style.left = `${pointer.position.x}px`
+			this.element.style.top = `${pointer.position.y - Space.pad}px`
+			this.element.style.transform = 'translate(-50%, -100%)'
+
+			// this.txt.setX(pointer.position.x)
+			// .setOrigin(0.5, 1)
+			// .setY(pointer.position.y - Space.pad)
 		}
 		// If there is a pin, go just to the right of that
 		else {
-			this.txt.setX(this.leftPin + Space.pad)
-			.setOrigin(0, 0.5)
-			.setY(pointer.position.y)
+			this.element.style.left = `${this.leftPin + Space.pad}px`
+			this.element.style.top = `${pointer.position.y}px`
+			this.element.style.transform = 'translate(0%, -50%)'
+
+			// this.txt.setX(this.leftPin + Space.pad)
+			// .setOrigin(0, 0.5)
+			// .setY(pointer.position.y)
 		}
+		return
 		
 		this.ensureOnScreen()
 	}
