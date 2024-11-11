1286dce6d6501d3a1d9d37933059bb8099958488
KaiGeffen <geffenkai@gmail.com>
2024-11-11 18:55:14 -0500

    Updated client webpack to not cache bust for dev

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,77 +1,71 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = (_, argv) => {
+  const isProd = argv.mode === 'production'
 
-module.exports = {
+  const config = {
     entry: './src/app.ts',
     module: {
-        rules: [
+      rules: [
         {
-            test: /\.tsx?$/,
-            use: 'ts-loader'
-        }
-        ]
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+        },
+      ],
     },
-    devServer: {
-        headers: {
-            "Access-Control-Allow-Origin": "216.193.175.49",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Headers": "*"
-        }
-    },
-    mode: 'development',
+    mode: isProd ? 'production' : 'development',
     resolve: {
-        extensions: [ '.ts', '.tsx', '.js' ],
-        alias: {
-            path: require.resolve("path-browserify")
-        }
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        path: require.resolve('path-browserify'),
+      },
     },
     output: {
-        libraryTarget: 'umd',
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+      libraryTarget: 'umd',
+      filename: isProd ? '[name].[contenthash].js' : '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
-  //   optimization: {
-  //    moduleIds: 'deterministic',
-  //     runtimeChunk: 'single',
-  //     splitChunks: {
-  //       cacheGroups: {
-  //         vendor: {
-  //           test: /[\\/]node_modules[\\/]/,
-  //           name: 'vendors',
-  //           chunks: 'all',
-  //         },
-  //       },
-  //     },
-  // },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
+    optimization: isProd
+      ? {
+          runtimeChunk: 'single',
+          splitChunks: {
             chunks: 'all',
             maxInitialRequests: Infinity,
             minSize: 0,
             cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name(module) {
-          // get the name. E.g. node_modules/packageName/not/this/part.js
-          // or node_modules/packageName
-                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-          // npm package names are URL-safe, but some servers don't like @ symbols
-                        return `npm.${packageName.replace('@', '')}`;
-                    },
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name(module) {
+                  const packageName = module.context.match(
+                    /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                  )[1]
+                  return `npm.${packageName.replace('@', '')}`
                 },
+              },
             },
-        },
-    },
+          },
+        }
+      : {},
     plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Caching',
-            template: 'template.html',
-            filename: '../index.html',
-        })
-        ],
+      new HtmlWebpackPlugin({
+        title: 'Caching',
+        template: 'template.html',
+        filename: '../index.html',
+      }),
+    ],
+  }
 
-};
+  if (!isProd) {
+    config.devServer = {
+      headers: {
+        'Access-Control-Allow-Origin': '216.193.175.49',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+    }
+  }
+
+  return config
+}
