7cde7b6ae7e20e67322944e3f52c8b4b1576a1c3
KaiGeffen <geffenkai@gmail.com>
2025-01-24 13:00:39 -0500

    Formatting flags.ts

--- client/src/settings/flags.ts
+++ client/src/settings/flags.ts
@@ -1,18 +1,18 @@
 var mobileCheck = require('is-mobile')
 
 export const Flags = {
-	// Running a local instance instead of on the server
-	local: location.port === '4949',
+  // Running a local instance instead of on the server
+  local: location.port === '4949',
 
-	// Is client connected to the internet
-	online: navigator.onLine,
+  // Is client connected to the internet
+  online: navigator.onLine,
 
-	// Include cards in development
-	devCards: new URLSearchParams(window.location.search).has('dev'),
+  // Include cards in development
+  devCards: new URLSearchParams(window.location.search).has('dev'),
 
-	// See cards in the opponent's hand
-	peek: new URLSearchParams(window.location.search).has('peek'),
+  // See cards in the opponent's hand
+  peek: new URLSearchParams(window.location.search).has('peek'),
 
-	// If user is on a mobile device
-	mobile: mobileCheck()
+  // If user is on a mobile device
+  mobile: mobileCheck(),
 }
