7d6ab7b5e9002cf9ff6faa055355e99ed08938c9
KaiGeffen <geffenkai@gmail.com>
2025-02-11 22:46:10 -0500

    Fixed recap being broken

--- server/src/network/match/match.ts
+++ server/src/network/match/match.ts
@@ -60,7 +60,7 @@ class Match {
         this.game.model.recentModels[player].forEach((state) =>
           ws.send({
             type: 'transmitState',
-            state: getClientGameModel(state, player, false),
+            state: getClientGameModel(state, player, true),
           }),
         )
 
