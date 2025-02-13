2c414ce492d298feafd8e37a16ec7433695f3886
KaiGeffen <geffenkai@gmail.com>
2025-02-12 21:12:05 -0500

    Added logging for match starting

--- server/src/network/matchQueue.ts
+++ server/src/network/matchQueue.ts
@@ -1,9 +1,7 @@
 import { parse } from 'url'
 import Card from '../../../shared/state/card'
 
-import {
-  TypedWebSocket,
-} from '../../../shared/network/typedWebSocket'
+import { TypedWebSocket } from '../../../shared/network/typedWebSocket'
 import { decodeDeck } from '../../../shared/codec'
 
 import PveMatch from './match/pveMatch'
@@ -49,85 +47,85 @@ class MatchQueue {
       // Start the match
       await match.notifyState()
     })
-    .on('initPvp', async (data) => {
-      // Clean up stale entries first
-      Object.keys(searchingPlayers).forEach(password => {
-        // TODO Websocket.OPEN is 1, but remote vs local views Websocket differently
-        if (searchingPlayers[password].ws.ws.readyState !== 1) {
-          delete searchingPlayers[password]
+      .on('initPvp', async (data) => {
+        // Clean up stale entries first
+        Object.keys(searchingPlayers).forEach((password) => {
+          // TODO Websocket.OPEN is 1, but remote vs local views Websocket differently
+          if (searchingPlayers[password].ws.ws.readyState !== 1) {
+            delete searchingPlayers[password]
+          }
+        })
+
+        // Check if there is another player, and they are still ready
+        const otherPlayer: WaitingPlayer = searchingPlayers[data.password]
+        if (otherPlayer) {
+          console.log(
+            'Match starting between players with decks:',
+            decodeDeck(data.deck),
+            otherPlayer.deck,
+          )
+
+          // Create a PvP match
+          const match = new PvpMatch(
+            ws,
+            data.uuid,
+            decodeDeck(data.deck),
+            data.avatar,
+            otherPlayer.ws,
+            otherPlayer.uuid,
+            otherPlayer.deck,
+            otherPlayer.avatar,
+          )
+
+          // registerEvents(socket, match, playerNumber)
+          delete searchingPlayers[data.password]
+          // TODO Maybe just delete the last one? Somehow don't lose to race conditions
+
+          registerEvents(ws, match, 0)
+          registerEvents(otherPlayer.ws, match, 1)
+
+          // Notify both players that they are connected
+          await match.notifyState()
+        } else {
+          // Queue the player with their information
+          const waitingPlayer = {
+            ws: ws,
+            uuid: data.uuid,
+            deck: decodeDeck(data.deck),
+            avatar: data.avatar,
+          }
+          searchingPlayers[data.password] = waitingPlayer
         }
       })
-
-      console.log('searching players are:', searchingPlayers)
-      
-      // Check if there is another player, and they are still ready
-      const otherPlayer: WaitingPlayer = searchingPlayers[data.password]
-      if (otherPlayer) {
-        // Create a PvP match
-        const match = new PvpMatch(
-          ws,
-          data.uuid,
-          decodeDeck(data.deck),
-          data.avatar,
-          otherPlayer.ws,
-          otherPlayer.uuid,
-          otherPlayer.deck,
-          otherPlayer.avatar,
-        )
-
-        // registerEvents(socket, match, playerNumber)
-        delete searchingPlayers[data.password]
-        // TODO Maybe just delete the last one? Somehow don't lose to race conditions
-
+      .on('initTutorial', async (data) => {
+        const match = new TutorialMatch(ws, data.num)
         registerEvents(ws, match, 0)
-        registerEvents(otherPlayer.ws, match, 1)
 
-        // Notify both players that they are connected
+        // Start the match
         await match.notifyState()
-      } else {
-        // Queue the player with their information
-        const waitingPlayer = {
-          ws: ws,
-          uuid: data.uuid,
-          deck: decodeDeck(data.deck),
-          avatar: data.avatar,
-        }
-        searchingPlayers[data.password] = waitingPlayer
-      }
-    })
-    .on('initTutorial', async (data) => {
-      const match = new TutorialMatch(ws, data.num)
-      registerEvents(ws, match, 0)
-
-      // Start the match
-      await match.notifyState()
-    })
+      })
   }
 }
 
 // Register each of the events that the server receives during a match
-function registerEvents(
-  ws: MatchServerWS,
-  match: Match,
-  playerNumber: number,
-) {
+function registerEvents(ws: MatchServerWS, match: Match, playerNumber: number) {
   ws.on('playCard', (data) => {
     const cardNum = data.cardNum
     match.doAction(playerNumber, cardNum)
   })
-  .on('mulligan', (data) => {
-    match.doMulligan(playerNumber, data.mulligan)
-  })
-  .on('passTurn', (data) => {
-    match.doAction(playerNumber, MechanicsSettings.PASS)
-  })
-  .on('exitMatch', (data) => {
-    match.doExit(ws)
-  })
-  .on('emote', (data) => {
-    const emote = 0 // TODO
-    match.signalEmote(playerNumber, emote)
-  })
+    .on('mulligan', (data) => {
+      match.doMulligan(playerNumber, data.mulligan)
+    })
+    .on('passTurn', (data) => {
+      match.doAction(playerNumber, MechanicsSettings.PASS)
+    })
+    .on('exitMatch', (data) => {
+      match.doExit(ws)
+    })
+    .on('emote', (data) => {
+      const emote = 0 // TODO
+      match.signalEmote(playerNumber, emote)
+    })
 }
 
 export default MatchQueue
