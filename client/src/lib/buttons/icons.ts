b489def071f24f3d01fe02637deb6d723f9351eb
KaiGeffen <geffenkai@gmail.com>
2025-02-13 10:08:18 -0500

    Pass button uses space key

--- client/src/lib/buttons/icons.ts
+++ client/src/lib/buttons/icons.ts
@@ -1,339 +1,355 @@
-import "phaser"
-import ContainerLite from 'phaser3-rex-plugins/plugins/containerlite.js';
-import { Space, Style, Color, Url, Flags } from '../../settings/settings'
+import 'phaser'
+import ContainerLite from 'phaser3-rex-plugins/plugins/containerlite.js'
+import {
+  Space,
+  Style,
+  Color,
+  Url,
+  Flags,
+  UserSettings,
+} from '../../settings/settings'
 import Button from './button'
-
 
 // Exported buttons
 class Options extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Options',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			},
-			sound: {
-				mute: true,
-			},
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Options',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+      sound: {
+        mute: true,
+      },
+    })
+  }
 }
 
 class SmallX extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		muteClick: boolean = false)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'SmallX',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			},
-			sound: {
-				mute: muteClick,
-			},
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+    muteClick: boolean = false,
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'SmallX',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+      sound: {
+        mute: muteClick,
+      },
+    })
+  }
 }
 
 class Share extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Share',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			}
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Share',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+    })
+  }
 }
 
 class Edit extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Edit',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			},
-			sound: {
-				mute: true,
-			},
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Edit',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+      sound: {
+        mute: true,
+      },
+    })
+  }
 }
 
 class Distribution extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Distribution',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			},
-			sound: {
-				mute: true,
-			},
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Distribution',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+      sound: {
+        mute: true,
+      },
+    })
+  }
 }
 
 class New extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'New',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			},
-			sound: {
-				mute: true,
-			},
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'New',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+      sound: {
+        mute: true,
+      },
+    })
+  }
 }
 
 class Recap extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Recap',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			}
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Recap',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+    })
+  }
 }
 
 class Skip extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Skip',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			}
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Skip',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+    })
+  }
 }
 
 class Arrow extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		theta: number, // In 90 degree increments from north
-		f: () => void = () => {})
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Arrow',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			}
-		})
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    theta: number, // In 90 degree increments from north
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Arrow',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+    })
 
-		this.icon.setRotation(theta * Math.PI/2)
-	}
+    this.icon.setRotation((theta * Math.PI) / 2)
+  }
 }
 
 // TODO Not really an icon, move somewhere else?
 class Pass extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			text: {
-				text: 'PASS',
-				interactive: false,
-				style: Style.pass,
-				offsetX: Flags.mobile ? -3 : 7,
-				offsetY: Flags.mobile ? 4 : 9,
-			},
-			icon: {
-				name: `${Flags.mobile ? 'Mobile' : ''}Sun`,
-				interactive: true,
-				circular: true,
-			},
-			callbacks: {
-				click: f
-			}
-		})
+  // Used in the tutorial to reduce the functionality while player is learning
+  tutorialSimplifiedPass = false
 
-		// Show a hint when hovered
-		// const s = 'Pass priority.\nOnce both players have passed in a row, the round ends.'
-		// super.makeHintable(s)
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      text: {
+        text: 'PASS',
+        interactive: false,
+        style: Style.pass,
+        offsetX: Flags.mobile ? -3 : 7,
+        offsetY: Flags.mobile ? 4 : 9,
+      },
+      icon: {
+        name: `${Flags.mobile ? 'Mobile' : ''}Sun`,
+        interactive: true,
+        circular: true,
+      },
+      callbacks: {
+        click: f,
+      },
+    })
 
-	enable() {
-		// For the tutorial, disable pass button
-		if (this.tutorialSimplifiedPass) {
-			this.icon.setAlpha(1)
-			return this
-		}
+    // If return hotkey is enabled, add a listener for the return key
+    this.scene.input.keyboard.on('keydown-SPACE', () => {
+      if (this.enabled && UserSettings._get('hotkeys')) {
+        this.onClick()
+      }
+    })
 
-		this.setText('PASS')
-		super.enable()
+    // Show a hint when hovered
+    // const s = 'Pass priority.\nOnce both players have passed in a row, the round ends.'
+    // super.makeHintable(s)
+  }
 
-		return this
-	}
+  enable() {
+    // For the tutorial, disable pass button
+    if (this.tutorialSimplifiedPass) {
+      this.icon.setAlpha(1)
+      return this
+    }
 
-	disable() {
-		// TODO Have this be on a cloud instead
-		// this.setText('THEIR\nTURN')
-		this.setText('')
-		super.disable()
+    this.setText('PASS')
+    super.enable()
 
-		return this
-	}
+    return this
+  }
 
-	// Used in the tutorial to reduce the functionality while player is learning
-	tutorialSimplifiedPass = false
+  disable() {
+    // TODO Have this be on a cloud instead
+    // this.setText('THEIR\nTURN')
+    this.setText('')
+    super.disable()
+
+    return this
+  }
 }
 
 class Moon extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			text: {
-				text: '',
-				interactive: false,
-				style: Style.moon,
-			},
-			icon: {
-				name: `${Flags.mobile ? 'Mobile' : ''}Moon`,
-				interactive: true,
-				circular: true,
-			},
-			callbacks: {
-				click: f
-			}
-		})
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      text: {
+        text: '',
+        interactive: false,
+        style: Style.moon,
+      },
+      icon: {
+        name: `${Flags.mobile ? 'Mobile' : ''}Moon`,
+        interactive: true,
+        circular: true,
+      },
+      callbacks: {
+        click: f,
+      },
+    })
 
-		// Rotate 180 since moon always viewed upside down
-		this.txt.setRotation(Math.PI)
-		.setAlign('center')
-	}
+    // Rotate 180 since moon always viewed upside down
+    this.txt.setRotation(Math.PI).setAlign('center')
+  }
 }
 
 // The search icon
 class Search extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Search',
-				interactive: true
-			},
-			callbacks: {
-				click: f
-			},
-			sound: {
-				mute: true,
-			},
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Search',
+        interactive: true,
+      },
+      callbacks: {
+        click: f,
+      },
+      sound: {
+        mute: true,
+      },
+    })
+  }
 }
 
 class Discord extends Button {
-	constructor(within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
-		x: number, y: number,
-		f: () => void = () => {},
-		)
-	{
-		super(within, x, y, 
-		{
-			icon: {
-				name: 'Discord',
-				interactive: true
-			},
-			callbacks: {
-				click: () => { window.open(Url.discord) }
-			}
-		})
-	}
+  constructor(
+    within: Phaser.Scene | Phaser.GameObjects.Container | ContainerLite,
+    x: number,
+    y: number,
+    f: () => void = () => {},
+  ) {
+    super(within, x, y, {
+      icon: {
+        name: 'Discord',
+        interactive: true,
+      },
+      callbacks: {
+        click: () => {
+          window.open(Url.discord)
+        },
+      },
+    })
+  }
 }
 
 // Export all of the available icons, which are subtype of buttons
 export default class Icons {
-	static Options = Options
-	static SmallX = SmallX
-	static Share = Share
-	static Edit = Edit
-	static Distribution = Distribution
-	static New = New
-	static Recap = Recap
-	static Skip = Skip
-	static Arrow = Arrow
-	static Pass = Pass
-	static Moon = Moon
-	static Search = Search
-	static Discord = Discord
+  static Options = Options
+  static SmallX = SmallX
+  static Share = Share
+  static Edit = Edit
+  static Distribution = Distribution
+  static New = New
+  static Recap = Recap
+  static Skip = Skip
+  static Arrow = Arrow
+  static Pass = Pass
+  static Moon = Moon
+  static Search = Search
+  static Discord = Discord
 }
