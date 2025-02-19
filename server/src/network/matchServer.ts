ae0cf86552b7a9a3efbe5b5e96c2390a6abeb794
KaiGeffen <geffenkai@gmail.com>
2025-02-19 15:00:15 -0500

    Removed excessive server connection notices

--- server/src/network/matchServer.ts
+++ server/src/network/matchServer.ts
@@ -1,14 +1,13 @@
 import { WebSocketServer } from 'ws'
 import MatchQueue from './matchQueue'
 
-import { URL, MATCH_PORT } from '../../../shared/network/settings'
+import { MATCH_PORT } from '../../../shared/network/settings'
 
 // Create the websocket server
 export default function createMatchServer() {
   const wss = new WebSocketServer({ port: MATCH_PORT })
 
   wss.on('connection', async (socket: WebSocket) => {
-    console.log('Match server connected')
     try {
       MatchQueue.enqueue(socket)
     } catch (e) {
